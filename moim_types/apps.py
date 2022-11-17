186eb41709c56336a233970a95e70699c8d6f414
SHY <ekdmbzzang@naver.com>
2022-11-17 21:00:28 +0900

    [FEAT] create app : moim_types

--- moim_types/apps.py
+++ moim_types/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CategoriesConfig(AppConfig):
+class MoimTypesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'categories'
+    name = 'moim_types'
