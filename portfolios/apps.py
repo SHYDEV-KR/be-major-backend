be50ec5e21c37fc1ead75c36549717eeaa5c32eb
SHY <ekdmbzzang@naver.com>
2022-11-17 21:05:22 +0900

    [FEAT] create app : portfolios

--- portfolios/apps.py
+++ portfolios/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MajorsConfig(AppConfig):
+class PortfoliosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'majors'
+    name = 'portfolios'
