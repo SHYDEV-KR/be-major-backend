22907d072bf742752d6aa5110c324853dcffb311
SHY <ekdmbzzang@naver.com>
2022-11-18 11:39:18 +0900

    [FEAT] topic model definition

--- topics/models.py
+++ topics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from common.models import CommonModel
 
 # Create your models here.
+class Topic(CommonModel):
+
+  ''' Topic Model Definition '''
+
+  name = models.CharField(max_length=100)
+  
+  def __str__(self):
+    return self.name
