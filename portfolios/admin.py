0a348cf8a2595e081ff7cacdc3dd7b2a3b64490f
SHY <ekdmbzzang@naver.com>
2022-11-18 11:40:31 +0900

    [FEAT] register portfolio admin

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Portfolio, Url, Career, Education
 
-# Register your models here.
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+
+  ''' Portfolio Admin Definition '''
+  pass
+
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+
+  ''' Url Admin Definition '''
+  pass
+
+
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+
+  ''' Career Admin Definition '''
+  pass
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+
+  ''' Education Admin Definition '''
+  pass
