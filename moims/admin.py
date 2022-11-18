5e914a11df3914dbcd847cc642b03106e1e9f74c
SHY <ekdmbzzang@naver.com>
2022-11-18 11:40:17 +0900

    [FEAT] register moim admin

--- moims/admin.py
+++ moims/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Moim, CrewJoin, LeaderApply
 
-# Register your models here.
+@admin.register(Moim)
+class MoimAdmin(admin.ModelAdmin):
+
+  ''' Moim Admin Definition '''
+
+  list_display = ("title", "moim_owner")
+  list_filter = ("moim_types", "topics")
+
+
+@admin.register(CrewJoin)
+class CrewJoinAdmin(admin.ModelAdmin):
+
+  ''' CrewJoin Admin Definition '''
+  pass
+
+
+@admin.register(LeaderApply)
+class LeaderApplyAdmin(admin.ModelAdmin):
+
+  ''' LeaderApply Admin Definition '''
+  pass
