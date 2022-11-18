15a9c4d8e2f536e46ddada39422537181f9abe97
SHY <ekdmbzzang@naver.com>
2022-11-18 11:40:55 +0900

    [FEAT] register user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+  ''' User Admin Definition '''
+
+  fieldsets = (
+    (
+      "User Information",
+      {
+        "fields": ("username", "password", "email", "phone_number", "is_phone_number_authenticated"),
+        "classes": ("wide",),
+      },
+    ),
+    (
+      "Permissions",
+      {
+        "fields": (
+          "is_active",
+          "is_staff",
+          "is_superuser",
+          "groups",
+          "user_permissions",
+        ),
+        "classes": ("collapse",),
+      },
+    ),
+    (
+      "Important Dates",
+      {
+        "fields": ("last_login", "date_joined"),
+        "classes": ("collapse",),
+      },
+    ),
+  )
+
+  list_display = ("username", "email")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+  ''' Profile Admin Definition '''
+
+  fieldsets = (
+    (
+      "Profile",
+      {
+        "fields": ("user", "gender", "date_of_birth" ,"is_leader"),
+        "classes": ("wide",),
+      },
+    ),
+  )
+  list_display = ("user", "is_leader")
