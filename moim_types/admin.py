778a32fb18168ad87e31da34016b52793fc1841e
SHY <ekdmbzzang@naver.com>
2022-11-18 11:40:07 +0900

    [FEAT] register moim type admin

--- moim_types/admin.py
+++ moim_types/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MoimType
 
-# Register your models here.
+@admin.register(MoimType)
+class MoimTypeAdmin(admin.ModelAdmin):
+
+  ''' MoimType Admin Definition '''
+  pass
