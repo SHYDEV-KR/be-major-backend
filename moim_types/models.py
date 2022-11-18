c0c5f1f9e9e60e4c7ff0d11d4e6a2cedf158097f
SHY <ekdmbzzang@naver.com>
2022-11-18 11:38:26 +0900

    [FEAT] moim type model definition

--- moim_types/models.py
+++ moim_types/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from common.models import CommonModel
 
 # Create your models here.
+class MoimType(CommonModel):
+
+  ''' MoimType Model Definition '''
+
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
