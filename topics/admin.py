a135b9f4eae642774501a4af99d7a1d15ed3d4b5
SHY <ekdmbzzang@naver.com>
2022-11-18 11:40:47 +0900

    [FEAT] register topics admin

--- topics/admin.py
+++ topics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Topic
 
-# Register your models here.
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+
+  ''' Topic Admin Definition '''
+  pass
