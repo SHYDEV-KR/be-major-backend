e363e0e94280286b330f6be61618dfa68617e3e6
SHY <ekdmbzzang@naver.com>
2022-11-20 12:27:07 +0900

    [FEAT] change portfolio models

--- portfolios/models.py
+++ portfolios/models.py
@@ -9,10 +9,13 @@ class Portfolio(CommonModel):
   owner = models.ForeignKey('users.User', on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
   topic = models.ManyToManyField('topics.Topic')
-  bio = models.TextField(max_length=1000)
-  urls = models.ManyToManyField('Url')
-  careers = models.ManyToManyField('Career')
-  education = models.ManyToManyField('Education')
+  bio = models.TextField(max_length=1000, null=True, blank=True)
+  urls = models.ManyToManyField('Url', null=True, blank=True)
+  careers = models.ManyToManyField('Career', null=True, blank=True)
+  education = models.ManyToManyField('Education', null=True, blank=True)
+
+  def __str__(self) -> str:
+    return self.title
 
 
 class Url(CommonModel):
@@ -33,7 +36,8 @@ class Career(CommonModel):
 
   description = models.CharField(max_length=100)
   start = models.DateField()
-  end = models.DateField()
+  end = models.DateField(null=True, blank=True)
+  is_current_job = models.BooleanField(default=False)
 
   def __str__(self):
     return f'[{self.start}~{self.end}] {self.description}'
