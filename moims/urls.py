8263de4908750adf26208e8d42605ef5be668c2d
SHY <ekdmbzzang@naver.com>
2022-11-21 19:21:41 +0900

    [FEAT] add moim urls path

--- moims/urls.py
+++ moims/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
   path("", views.MoimList.as_view()),
   path("<int:moim_id>", views.MoimDetail.as_view()),
+  path("<int:moim_id>/crew-join", views.CrewJoin.as_view()),
+  path("<int:moim_id>/leader-apply", views.LeaderApply.as_view()),
+  path("<int:moim_id>/owner", views.MoimDetailForOwner.as_view()),
+  path("<int:moim_id>/owner/change-state", views.ChangeMoimCloseState.as_view()),
+  path("<int:moim_id>/owner/choose-leader", views.ChooseLeader.as_view()),
 ]
