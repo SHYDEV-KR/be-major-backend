f8b9130fb0147bd8eb16831ab04704b82d4e6019
SHY <ekdmbzzang@naver.com>
2022-11-21 19:21:52 +0900

    [FEAT] moim views

--- moims/views.py
+++ moims/views.py
@@ -1,15 +1,16 @@
 from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound, NotAuthenticated, PermissionDenied, ParseError
+from rest_framework.exceptions import NotFound, NotAuthenticated, PermissionDenied, ParseError, ValidationError
 from rest_framework.response import Response
 from .models import Moim
 from topics.models import Topic
+from portfolios.models import Portfolio
 from moim_types.models import MoimType
 from users.models import User
-from .serializers import MoimDetailSerializer, MoimPublicDetailSerializer, MoimMinimalSerializer
-from rest_framework.status import HTTP_406_NOT_ACCEPTABLE
+from .serializers import CrewJoinSerializer, MoimDetailSerializer, MoimPublicDetailSerializer, MoimMinimalSerializer, LeaderApplySerializer
+from rest_framework.status import HTTP_406_NOT_ACCEPTABLE, HTTP_204_NO_CONTENT
 from django.shortcuts import get_object_or_404
 from django.db import transaction
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class MoimList(APIView):
@@ -60,10 +61,15 @@ class MoimList(APIView):
           try:
             leader_id = request.data.get("leader")
             leader = User.objects.get(pk=leader_id)
-            new_moim.leader.add(leader)
+            if leader == request.user:
+              raise ValidationError
+          except ValidationError:
+            raise ParseError("Cannot be leader")
           except Exception as e:
             raise ParseError("Leader not found.")
-
+          
+          new_moim.leader = leader
+          new_moim.save()
 
         return Response(
           MoimDetailSerializer(new_moim).data,
@@ -84,12 +90,17 @@ class MoimDetail(APIView):
       raise NotFound
 
   def get(self, request, moim_id):
+    
+    ''' Show Public Moim Detail '''
+
     moim = self.get_object(moim_id)
     serializer = MoimPublicDetailSerializer(moim)
     return Response(serializer.data)
 
 
 class MoimDetailForOwner(APIView):
+  permission_classes = [IsAuthenticated]
+
   def get_object(self, id):
     try:
       return Moim.objects.get(pk=id)
@@ -97,14 +108,175 @@ class MoimDetailForOwner(APIView):
       raise NotFound
 
   def get(self, request, moim_id):
+
+    ''' Show Private Moim Detail '''
+
     moim = self.get_object(moim_id)
-    serializer = MoimSerializer(moim)
+    
+    #if moim.moim_owner != request.user:
+    #  raise PermissionDenied
+    
+    serializer = MoimDetailSerializer(moim)
     return Response(serializer.data)
 
 
   def delete(self, request, moim_id):
     moim = self.get_object(moim_id)
-    if not request.user.is_authenticated:
-      raise NotAuthenticated
-    if moim.owner != request.user:
+    if moim.moim_owner != request.user:
       raise PermissionDenied
+
+    moim.delete()
+    return Response(status=HTTP_204_NO_CONTENT)
+
+
+class ChangeMoimCloseState(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get_object(self, id):
+    try:
+      return Moim.objects.get(pk=id)
+    except Moim.DoesNotExist:
+      raise NotFound
+
+  def get(self, request, moim_id):
+
+    ''' Change Moim Close State '''
+
+    moim = self.get_object(moim_id)
+    
+    if moim.moim_owner != request.user:
+      raise PermissionDenied
+
+    if moim.crewjoin_set.count() < moim.min_participants:
+      raise ValidationError("Cannot close moim : not enough number of participants.")
+
+    if moim.is_closed:
+      moim.is_closed = False
+    else:
+      moim.is_closed = True
+
+    moim.save()
+
+    serializer = MoimDetailSerializer(moim)
+    return Response(serializer.data)
+
+
+class ChooseLeader(APIView):
+  permission_classes = [IsAuthenticated]
+
+  def get_object(self, id):
+    try:
+      return Moim.objects.get(pk=id)
+    except Moim.DoesNotExist:
+      raise NotFound
+
+  def post(self, request, moim_id):
+
+    ''' Choose Leader of Moim '''
+
+    moim = self.get_object(moim_id)
+    
+    if moim.moim_owner != request.user:
+      raise PermissionDenied
+    
+    if not request.data.get("leader"):
+      raise ParseError
+
+    try:
+      leader_id = request.data.get("leader")
+      leader = User.objects.get(pk=leader_id)
+      if leader == request.user:
+        raise ValidationError
+    except ValidationError:
+      raise ParseError("Cannot be leader")
+    except Exception as e:
+      raise ParseError("Leader not found.")
+
+    if not moim.leaderapply_set.filter(owner=leader).exists():
+      raise ParseError("Leader didn't apply.")
+      
+    moim.leader = leader
+    moim.save()
+
+    serializer = MoimDetailSerializer(moim)
+    return Response(serializer.data)
+
+
+
+class CrewJoin(APIView):
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  def get_object(self, id):
+    try:
+      return Moim.objects.get(pk=id)
+    except Moim.DoesNotExist:
+      raise NotFound
+
+  def post(self, request, moim_id):
+
+    ''' Crew : Join Moim '''
+
+    moim = self.get_object(moim_id)
+    if moim.crewjoin_set.filter(owner=request.user).exists():
+      raise ParseError("Already Joined this moim.")
+    if moim.is_closed:
+      raise ParseError("Moim closed.")
+    if moim.leaderapply_set.filter(owner=request.user).exists():
+      raise ParseError("Already Applied to this moim as leader.")
+    if moim.crewjoin_set.count() >= moim.max_participants:
+      raise ParseError("No more joins available.")
+
+    serializer = CrewJoinSerializer(data=request.data)
+    if serializer.is_valid():
+      new_crew_join = serializer.save(owner=request.user, moim=moim)
+      return Response(
+          CrewJoinSerializer(new_crew_join).data,
+        )
+    else:
+      return Response(serializer.errors, status=HTTP_406_NOT_ACCEPTABLE)
+
+
+class LeaderApply(APIView):
+  permission_classes = [IsAuthenticatedOrReadOnly]
+
+  def get_object(self, id):
+    try:
+      return Moim.objects.get(pk=id)
+    except Moim.DoesNotExist:
+      raise NotFound
+
+  def post(self, request, moim_id):
+
+    ''' Leader : Apply to Moim '''
+
+    moim = self.get_object(moim_id)
+    
+    if moim.leaderapply_set.filter(owner=request.user).exists():
+      raise ParseError("Already Applied to this moim.")
+    if moim.crewjoin_set.filter(owner=request.user).exists():
+      raise ParseError("Already Joined this moim as crew.")
+    if moim.is_closed:
+      raise ParseError("Moim closed.")
+
+    serializer = LeaderApplySerializer(data=request.data)
+    if serializer.is_valid():
+      with transaction.atomic():
+        new_leader_apply = serializer.save(owner=request.user, moim=moim)
+        try:
+          portfolios = request.data.get("portfolio")
+          for portfolio_id in portfolios:
+            portfolio = Portfolio.objects.get(pk=portfolio_id)
+            if portfolio.owner != request.user:
+              raise ValidationError
+            new_leader_apply.portfolio.add(portfolio)
+        except ValidationError:
+          raise ParseError("Not owner of portfolio.")
+        except Exception as e:
+          raise ParseError("Portfolio not found.")
+
+
+        return Response(
+            LeaderApplySerializer(new_leader_apply).data,
+          )
+    else:
+      return Response(serializer.errors, status=HTTP_406_NOT_ACCEPTABLE)
