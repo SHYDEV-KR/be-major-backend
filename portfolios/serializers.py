471322c6dcc5ac8b26ed7995894a8d927da49361
SHY <ekdmbzzang@naver.com>
2022-11-22 15:06:45 +0900

    [FEAT] portfolio serializer

--- portfolios/serializers.py
+++ portfolios/serializers.py
@@ -1,13 +1,50 @@
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
-from .models import Portfolio
+from .models import Career, Education, Portfolio, Url
 
 class PortfolioMinimalSerializer(ModelSerializer):
-  topic = SlugRelatedField(
+  topics = SlugRelatedField(
         many=True,
         read_only=True,
         slug_field='name'
     )
   class Meta:
       model = Portfolio
-      fields = ("id", "title", "topic", "bio")
+      fields = ("id", "title", "topics", "bio")
 
+
+class UrlSerializer(ModelSerializer):
+  class Meta:
+      model = Url
+      fields = "__all__"
+
+class CareerSerializer(ModelSerializer):
+  class Meta:
+      model = Career
+      fields = "__all__"
+
+class EducationSerializer(ModelSerializer):
+  class Meta:
+      model = Education
+      fields = "__all__"
+
+
+class PortfolioDetailSerializer(ModelSerializer):
+  topics = SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+  )
+
+  urls = UrlSerializer(many=True, read_only=True)
+  careers = CareerSerializer(many=True, read_only=True)
+  education = EducationSerializer(many=True, read_only=True)
+
+  class Meta:
+    model = Portfolio
+    fields = "__all__"
+
+
+class PortfolioViewSetSerializer(ModelSerializer):
+  class Meta:
+    model = Portfolio
+    fields = "__all__"
