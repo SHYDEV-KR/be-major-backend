5cc6ce486d1f91d63a20a832d8339c651906140b
SHY <ekdmbzzang@naver.com>
2022-11-22 15:07:02 +0900

    [FEAT] portfolio urls (viewset)

---
+++ portfolios/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+  path("", views.PortfolioViewSet.as_view({
+    "post": "create",
+  })),
+  path("<int:pk>", views.PortfolioViewSet.as_view({
+    "get" : "retrieve",
+    "put": "partial_update",
+    "delete": "destroy"
+  })),
+  path("careers/<int:pk>", views.CareerViewSet.as_view({
+    "get" : "retrieve",
+    "put": "partial_update",
+    "delete": "destroy"
+  })),
+
+  path("urls/<int:pk>", views.UrlViewSet.as_view({
+    "get" : "retrieve",
+    "put": "partial_update",
+    "delete": "destroy"
+  })),
+  
+  path("education/<int:pk>", views.EducationViewSet.as_view({
+    "get" : "retrieve",
+    "put": "partial_update",
+    "delete": "destroy"
+  })),
+]
