5a641fee683874d1de3424948a01b3844078cbb6
SHY <ekdmbzzang@naver.com>
2022-12-01 05:22:45 +0900

    [FEAT] moim views

--- moims/views.py
+++ moims/views.py
@@ -1,12 +1,14 @@
+from datetime import datetime
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound, NotAuthenticated, PermissionDenied, ParseError, ValidationError
 from rest_framework.response import Response
 from .models import Moim
+from . import models
 from topics.models import Topic
 from portfolios.models import Portfolio
 from moim_types.models import MoimType
 from users.models import User
-from .serializers import CrewJoinSerializer, MoimDetailSerializer, MoimPublicDetailSerializer, MoimMinimalSerializer, LeaderApplySerializer
+from .serializers import CrewJoinMinimalSerializer, CrewJoinSerializer, LeaderApplyMinimalSerializer, MoimDetailSerializer, MoimPublicDetailSerializer, MoimMinimalSerializer, LeaderApplySerializer
 from rest_framework.status import HTTP_406_NOT_ACCEPTABLE, HTTP_204_NO_CONTENT
 from django.shortcuts import get_object_or_404
 from django.db import transaction
@@ -21,7 +23,7 @@ class MoimList(APIView):
 
     ''' Show Moim List '''
 
-    all_moims = Moim.objects.all()
+    all_moims = Moim.objects.filter(expiration_date__gte=datetime.now().strftime('%Y-%m-%d')).order_by("-created_at")
     serializer = MoimMinimalSerializer(
       all_moims, 
       many=True, 
@@ -37,21 +39,21 @@ class MoimList(APIView):
 
     def add_topics_to_moim_from_request(request, moim):
       topics = request.data.get("topics")
-      for topic_id in topics:
-        topic = Topic.objects.get(pk=topic_id)
+      for topic_name in topics:
+        topic = Topic.objects.get(name=topic_name)
         moim.topics.add(topic)
 
 
     def add_moim_types_to_moim_from_request(request, moim):
       moim_types = request.data.get("moim_types")
-      for moim_type_id in moim_types:
-        moim_type = MoimType.objects.get(pk=moim_type_id)
+      for moim_type_name in moim_types:
+        moim_type = MoimType.objects.get(name=moim_type_name)
         moim.moim_types.add(moim_type)
 
 
     def create_moim_with_response(serializer):
       with transaction.atomic(): ## 오류없이 통과하면 코드 한 번에 실행
-        new_moim = serializer.save(owner=request.user)
+        new_moim = serializer.save(owner=request.user.profile)
         try:
           add_topics_to_moim_from_request(request, new_moim)
         except Exception:
@@ -62,28 +64,17 @@ class MoimList(APIView):
         except Exception as e:
           raise ParseError("MoimType not found.")
 
-        if request.data.get("leader"):
-          try:
-            leader_id = request.data.get("leader")
-            leader = User.objects.get(pk=leader_id)
-            if leader == request.user:
-              raise ValidationError
-          except ValidationError:
-            raise ParseError("Cannot be leader")
-          except Exception as e:
-            raise ParseError("Leader not found.")
-          
-          new_moim.leader = leader
-          new_moim.save()
-
         return Response(
-          MoimDetailSerializer(new_moim).data,
+          MoimDetailSerializer(new_moim, context={"request": request}).data,
         )
+
+    print(request.data)
 
     serializer = MoimDetailSerializer(data=request.data)
     if serializer.is_valid():
       return create_moim_with_response(serializer)
     else:
+      print(serializer.errors)
       return Response(serializer.errors, status=HTTP_406_NOT_ACCEPTABLE)
 
 
@@ -118,7 +109,7 @@ class MoimDetailForOwner(APIView):
 
     moim = self.get_object(moim_id)
     
-    if moim.owner != request.user:
+    if moim.owner.id != request.user.profile.id:
       raise PermissionDenied
     
     serializer = MoimDetailSerializer(moim, context={'request':request})
@@ -127,7 +118,7 @@ class MoimDetailForOwner(APIView):
 
   def delete(self, request, moim_id):
     moim = self.get_object(moim_id)
-    if moim.owner != request.user:
+    if moim.owner.id != request.user.profile.id:
       raise PermissionDenied
 
     moim.delete()
@@ -210,33 +201,48 @@ class ChooseLeader(APIView):
 class CrewJoin(APIView):
   permission_classes = [IsAuthenticatedOrReadOnly]
 
-  def get_object(self, id):
+  def get_moim_object(self, id):
     try:
       return Moim.objects.get(pk=id)
     except Moim.DoesNotExist:
       raise NotFound
 
+  def get_crew_join_object(self, id):
+    try:
+      return models.CrewJoin.objects.get(pk=id)
+    except models.CrewJoin.DoesNotExist:
+      raise NotFound
+
+  def get(self, request, crew_join_id):
+      
+    ''' Show Crew Join Detail '''
+
+    crew_join = self.get_crew_join_object(crew_join_id)
+    serializer = CrewJoinMinimalSerializer(crew_join, context={'request':request})
+    return Response(serializer.data)
+
   def post(self, request, moim_id):
 
     ''' Crew : Join Moim '''
 
-    moim = self.get_object(moim_id)
-    if moim.crewjoin_set.filter(owner=request.user).exists():
+    moim = self.get_moim_object(moim_id)
+    if moim.crewjoin_set.filter(owner=request.user.profile).exists():
       raise ParseError("Already Joined this moim.")
     if moim.is_closed:
       raise ParseError("Moim closed.")
-    if moim.leaderapply_set.filter(owner=request.user).exists():
+    if moim.leaderapply_set.filter(owner=request.user.profile).exists():
       raise ParseError("Already Applied to this moim as leader.")
     if moim.crewjoin_set.count() >= moim.max_participants:
       raise ParseError("No more joins available.")
 
-    serializer = CrewJoinSerializer(data=request.data)
+    serializer = CrewJoinSerializer(data=request.data, context={'request':request})
     if serializer.is_valid():
-      new_crew_join = serializer.save(owner=request.user, moim=moim)
+      new_crew_join = serializer.save(owner=request.user.profile, moim=moim)
       return Response(
-          CrewJoinSerializer(new_crew_join).data,
+          CrewJoinSerializer(new_crew_join, context={'request':request}).data,
         )
     else:
+      print(serializer.errors)
       return Response(serializer.errors, status=HTTP_406_NOT_ACCEPTABLE)
 
 
@@ -248,6 +254,21 @@ class LeaderApply(APIView):
       return Moim.objects.get(pk=id)
     except Moim.DoesNotExist:
       raise NotFound
+
+  def get_leader_apply_object(self, id):
+    print(LeaderApply)
+    try:
+      return models.LeaderApply.objects.get(pk=id)
+    except models.LeaderApply.DoesNotExist:
+      raise NotFound
+
+  def get(self, request, leader_apply_id):
+      
+    ''' Show Crew Join Detail '''
+
+    leader_apply = self.get_leader_apply_object(leader_apply_id)
+    serializer = LeaderApplyMinimalSerializer(leader_apply, context={'request':request})
+    return Response(serializer.data)
 
   def post(self, request, moim_id):
 
@@ -261,8 +282,6 @@ class LeaderApply(APIView):
       raise ParseError("Already Joined this moim as crew.")
     if moim.is_closed:
       raise ParseError("Moim closed.")
-    if not request.user.profile.is_leader:
-      raise ParseError("Not A leader.")
 
     serializer = LeaderApplySerializer(data=request.data)
     if serializer.is_valid():
