3be6fce5e51073f4dc401ae4c19360107ee7be4e
SHY <ekdmbzzang@naver.com>
2022-12-01 05:21:24 +0900

    [FEAT] moimtype manager

--- moim_types/models.py
+++ moim_types/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from common.models import CommonModel
 
 # Create your models here.
+class MoimTypeManager(models.Manager):
+  def create_moim_type(self, name):
+    moim_type = self.create(name=name)
+    return moim_type
+
 class MoimType(CommonModel):
 
   ''' MoimType Model Definition '''
 
   name = models.CharField(max_length=100)
+  objects = MoimTypeManager()
 
   def __str__(self):
     return self.name
