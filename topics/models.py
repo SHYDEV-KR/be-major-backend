d9433c777d6ae211dd48303f9cf6ecfd34e87f31
SHY <ekdmbzzang@naver.com>
2022-12-01 05:23:13 +0900

    [FEAT] topic

--- topics/models.py
+++ topics/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from common.models import CommonModel
 
 # Create your models here.
+
+class TopicManager(models.Manager):
+  def create_topic(self, name):
+    topic = self.create(name=name)
+    return topic
+
 class Topic(CommonModel):
 
   ''' Topic Model Definition '''
 
   name = models.CharField(max_length=100)
+  objects = TopicManager()
   
   def __str__(self):
     return self.name
