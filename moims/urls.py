a56991d3d82c5a7222e3beaca2913408b943504d
SHY <ekdmbzzang@naver.com>
2022-12-01 05:22:07 +0900

    [FEAT] moim urls

--- moims/urls.py
+++ moims/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
   path("", views.MoimList.as_view()),
-  path("<int:moim_id>", views.MoimDetail.as_view()),
-  path("<int:moim_id>/crew-join", views.CrewJoin.as_view()),
-  path("<int:moim_id>/leader-apply", views.LeaderApply.as_view()),
-  path("<int:moim_id>/owner", views.MoimDetailForOwner.as_view()),
+  path("crew-join/<int:crew_join_id>", views.CrewJoin.as_view()),
+  path("leader-apply/<int:leader_apply_id>", views.LeaderApply.as_view()),
   path("<int:moim_id>/owner/change-state", views.ChangeMoimCloseState.as_view()),
   path("<int:moim_id>/owner/choose-leader", views.ChooseLeader.as_view()),
+  path("<int:moim_id>/owner", views.MoimDetailForOwner.as_view()),
+  path("<int:moim_id>/crew-join", views.CrewJoin.as_view()),
+  path("<int:moim_id>/leader-apply", views.LeaderApply.as_view()),
+  path("<int:moim_id>", views.MoimDetail.as_view()),
 ]
